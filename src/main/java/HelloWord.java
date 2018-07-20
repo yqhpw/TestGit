08bd2d8eb77ff38aca3bef9aeb28f00a72d85c1d
yqhpw <yqhp_ww@163.com>
2018-07-20 19:49:05 +0800

    进行测试02

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -8,5 +8,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word !!");
         System.out.println("贼坑！！！！！！！！！！！");
+        System.out.println("测试是否可以监控到");
     }
 }
